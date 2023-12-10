b67ef72f506280e4064ada6d66c86b04fcbf88eb
Wisnu Agung Prasetyo <wisungyo@gmail.com>
2023-12-10 17:06:41 +0700

    feat: add response copy

---
+++ app/constants/response.ts
@@ -0,0 +1,171 @@
+/**
+ *
+ *
+ *  SAMPLE OF RESPONSES
+ *  IN CASE SOMETIMES API IS DOWN OR INACCESSIBLE ANYMORE
+ *
+ *  CURRENT API SOURCE:
+ *  https://shikimori.one/api/doc/1.0
+ *
+ *
+ */
+
+/**
+ *  page: {number}
+ *  limit: 8
+ *  order: popularity
+ */
+const getData = [
+  {
+    id: 16498,
+    name: "Shingeki no Kyojin",
+    russian: "Атака титанов",
+    image: {
+      original: "/system/animes/original/16498.jpg?1701440173",
+      preview: "/system/animes/preview/16498.jpg?1701440173",
+      x96: "/system/animes/x96/16498.jpg?1701440173",
+      x48: "/system/animes/x48/16498.jpg?1701440173",
+    },
+    url: "/animes/16498-shingeki-no-kyojin",
+    kind: "tv",
+    score: "8.54",
+    status: "released",
+    episodes: 25,
+    episodes_aired: 25,
+    aired_on: "2013-04-07",
+    released_on: "2013-09-29",
+  },
+  {
+    id: 1535,
+    name: "Death Note",
+    russian: "Тетрадь смерти",
+    image: {
+      original: "/assets/globals/missing_original.jpg?1701405586",
+      preview: "/assets/globals/missing_preview.jpg?1701405586",
+      x96: "/assets/globals/missing_x96.jpg?1701405586",
+      x48: "/assets/globals/missing_x48.jpg?1701405586",
+    },
+    url: "/animes/1535-death-note",
+    kind: "tv",
+    score: "8.62",
+    status: "released",
+    episodes: 37,
+    episodes_aired: 0,
+    aired_on: "2006-10-04",
+    released_on: "2007-06-27",
+  },
+  {
+    id: 5114,
+    name: "Fullmetal Alchemist: Brotherhood",
+    russian: "Стальной алхимик: Братство",
+    image: {
+      original: "/system/animes/original/5114.jpg?1701409327",
+      preview: "/system/animes/preview/5114.jpg?1701409327",
+      x96: "/system/animes/x96/5114.jpg?1701409327",
+      x48: "/system/animes/x48/5114.jpg?1701409327",
+    },
+    url: "/animes/z5114-fullmetal-alchemist-brotherhood",
+    kind: "tv",
+    score: "9.09",
+    status: "released",
+    episodes: 64,
+    episodes_aired: 0,
+    aired_on: "2009-04-05",
+    released_on: "2010-07-04",
+  },
+  {
+    id: 30276,
+    name: "One Punch Man",
+    russian: "Ванпанчмен",
+    image: {
+      original: "/system/animes/original/30276.jpg?1701431501",
+      preview: "/system/animes/preview/30276.jpg?1701431501",
+      x96: "/system/animes/x96/30276.jpg?1701431501",
+      x48: "/system/animes/x48/30276.jpg?1701431501",
+    },
+    url: "/animes/z30276-one-punch-man",
+    kind: "tv",
+    score: "8.5",
+    status: "released",
+    episodes: 12,
+    episodes_aired: 12,
+    aired_on: "2015-10-05",
+    released_on: "2015-12-21",
+  },
+  {
+    id: 11757,
+    name: "Sword Art Online",
+    russian: "Мастера Меча Онлайн",
+    image: {
+      original: "/system/animes/original/11757.jpg?1701442673",
+      preview: "/system/animes/preview/11757.jpg?1701442673",
+      x96: "/system/animes/x96/11757.jpg?1701442673",
+      x48: "/system/animes/x48/11757.jpg?1701442673",
+    },
+    url: "/animes/z11757-sword-art-online",
+    kind: "tv",
+    score: "7.2",
+    status: "released",
+    episodes: 25,
+    episodes_aired: 25,
+    aired_on: "2012-07-08",
+    released_on: "2012-12-23",
+  },
+  {
+    id: 31964,
+    name: "Boku no Hero Academia",
+    russian: "Моя геройская академия",
+    image: {
+      original: "/system/animes/original/31964.jpg?1701401046",
+      preview: "/system/animes/preview/31964.jpg?1701401046",
+      x96: "/system/animes/x96/31964.jpg?1701401046",
+      x48: "/system/animes/x48/31964.jpg?1701401046",
+    },
+    url: "/animes/31964-boku-no-hero-academia",
+    kind: "tv",
+    score: "7.88",
+    status: "released",
+    episodes: 13,
+    episodes_aired: 13,
+    aired_on: "2016-04-03",
+    released_on: "2016-06-26",
+  },
+  {
+    id: 38000,
+    name: "Kimetsu no Yaiba",
+    russian: "Клинок, рассекающий демонов",
+    image: {
+      original: "/system/animes/original/38000.jpg?1701420423",
+      preview: "/system/animes/preview/38000.jpg?1701420423",
+      x96: "/system/animes/x96/38000.jpg?1701420423",
+      x48: "/system/animes/x48/38000.jpg?1701420423",
+    },
+    url: "/animes/z38000-kimetsu-no-yaiba",
+    kind: "tv",
+    score: "8.49",
+    status: "released",
+    episodes: 26,
+    episodes_aired: 26,
+    aired_on: "2019-04-06",
+    released_on: "2019-09-28",
+  },
+  {
+    id: 20,
+    name: "Naruto",
+    russian: "Наруто",
+    image: {
+      original: "/system/animes/original/20.jpg?1701428801",
+      preview: "/system/animes/preview/20.jpg?1701428801",
+      x96: "/system/animes/x96/20.jpg?1701428801",
+      x48: "/system/animes/x48/20.jpg?1701428801",
+    },
+    url: "/animes/z20-naruto",
+    kind: "tv",
+    score: "7.99",
+    status: "released",
+    episodes: 220,
+    episodes_aired: 0,
+    aired_on: "2002-10-03",
+    released_on: "2007-02-08",
+  },
+];
