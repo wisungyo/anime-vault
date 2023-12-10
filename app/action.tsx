69a9f6781e6618155f5fa3db52f8514e21e3d431
Wisnu Agung Prasetyo <wisungyo@gmail.com>
2023-12-10 14:07:51 +0700

    fix: remove console

--- app/action.tsx
+++ app/action.tsx
@@ -9,8 +9,6 @@ export const fetchAnime = async (page: number) => {
 
   const data = await res.json();
 
-  console.log(data);
-
   return data.map((item: AnimeProp, index: number) => (
     <AnimeCard key={item.id} anime={item} index={index} />
   ));
